4e15cefb59facdf8a25fcab5f19d594150b68b72
BooMajka <80685266+BooMajka@users.noreply.github.com>
2021-12-21 16:02:17 -0600

    Update 4-challenge1.js

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -3,7 +3,18 @@ const theOneFunc = () => {};
 setTimeout(theOneFunc, 4 * 1000);
 
 // Hello after 4 seconds
-
+const fourSecFunc = () => {
+  console.log("Hello after 4 seconds");
+}
+setTimeout(fourSecFun, 4000);
 // Hello after 8 seconds
-
+const fourSecFunc = () => {
+  console.log("Hello after 8 seconds");
+}
+setTimeout(fourSecFun, 8000);
 // You can define only ONE function
+const callFunc = delay => {
+  console.log("Hello after " + delay + "seconds");
+};
+setTimeout(callFunc, 4000, 4);
+ setTimeout(callFunc, 8000, 8);
